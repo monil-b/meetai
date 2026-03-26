74e525b7e88943ecd218088d317f5eff5268ee3e
Harshal Gondaliya <harshalgondaliya1234@gmail.com>
2026-03-26 10:45:44 +0530

    06:dashboard-sidebar

--- src/modules/auth/ui/views/sign-up-view.tsx
+++ src/modules/auth/ui/views/sign-up-view.tsx
@@ -195,7 +195,7 @@ export const SignUpView = () => {
                   </Alert>
                 )}
                 <Button disabled={pending} type='submit' className='w-full'>
-                  Sign In
+                  Sign Up
                 </Button>
                 <div className='after:border-border relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t'>
                   <span className='bg-card text-muted-foreground relative z-10 px-2'>
@@ -234,7 +234,7 @@ export const SignUpView = () => {
               </div>
             </form>
           </Form>
-          <div className='bg-radial from-green-700 to-green-900 relative hidden md:flex flex-col gap-y-4 items-center justify-center'>
+          <div className='bg-radial from-sidebar-accent to-sidebar relative hidden md:flex flex-col gap-y-4 items-center justify-center'>
             <img src='/logo.svg' alt='Image' className='h-[92px] w-[92px]' />
             <p className='text-2xl font-semibold text-white'>Meet.AI</p>
           </div>
