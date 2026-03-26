5287bd562e67318fc2a3c90fbb02a15bf2b83868
Om Rana <ranaom1112@gmail.com>
2026-03-26 19:45:27 +0530

    Dashboard navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,14 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter } from "next/font/google";
 import "./globals.css";
 import { TooltipProvider } from "@/components/ui/tooltip";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const inter = Inter({
+ 
   subsets: ["latin"],
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+
 
 export const metadata: Metadata = {
   title: "MeetAI",
@@ -26,7 +23,7 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      className={`${inter.className} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col bg-background text-foreground">
         <TooltipProvider>
--- src/modules/dashboard/ui/components/dashboard-user-button.tsx
+++ src/modules/dashboard/ui/components/dashboard-user-button.tsx
@@ -10,13 +10,24 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu';
+import{
+  Drawer,
+  DrawerContent,
+  DrawerDescription,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+}from '@/components/ui/drawer';
 import { GeneratedAvatar } from '@/components/ui/generated-avatar';
 import { da } from 'date-fns/locale';
 import { Chevron } from 'react-day-picker';
 import { ChevronDownIcon, CreditCardIcon, LogOutIcon } from 'lucide-react';
+import { Button } from '@/components/ui/button';
 
 export const DashboardUserButton = () => {
   const router = useRouter();
+  const isMobile = useIsMobile();
   const { data, isPending } = authClient.useSession();
 
   const onLogout = () => {
@@ -32,12 +43,62 @@ export const DashboardUserButton = () => {
   if (isPending || !data?.user) {
     return null;
   }
+  if (isMobile) {
+    return (
+     <Drawer>
+      <DrawerTrigger className='rounded-lg border border-border/10 p-3 w-full
+      flex items-center justify-between bg-white/5 hover:bg-white/10 overflow-hidden gap-x-2'>
+        {data.user.image ? (
+          <Avatar>
+            <AvatarImage src={data.user.image} />
+          </Avatar>
+        ) : (
+          <GeneratedAvatar
+            seed={data.user.name}
+            variant='initials'
+            className='size-9 mr-3'
+          />
+        )}
+
+        <div className='flex flex-col gap-0.5 text-left overflow-hidden flex-1 min-w-0'>
+          <p className='text-sm truncate w-full'>{data.user.name}</p>
+          <p className='text-xs truncate w-full'>{data.user.email}</p>
+        </div>
+
+        <ChevronDownIcon className='size-4 shrink-0' />
+      </DrawerTrigger>
+      <DrawerContent>
+        <DrawerHeader>
+          <DrawerTitle>{data.user.name}</DrawerTitle>
+          <DrawerDescription>{data.user.email}</DrawerDescription>
+        </DrawerHeader>
+        <DrawerFooter>
+          <Button
+            variant="outline"
+            onClick={() => {}}
+          >
+            <CreditCardIcon class-name="size-4 text-black"/>
+            Billing
+            
+          </Button>
+          <Button
+            variant="outline"
+            onClick={onLogout}
+          >
+            <LogOutIcon class-name="size-4 text-black"/>
+            Logout
+            
+          </Button>
+        </DrawerFooter>
+      </DrawerContent>
+     </Drawer>
+    );
+  }
   return (
     <DropdownMenu>
       <DropdownMenuTrigger
         className='rounded-lg border border-border/10 p-3 w-full
-                flex items-center justify-between bg-white/5 hover:bg-white/10 overflow-hidden'
-      >
+        flex items-center justify-between bg-white/5 hover:bg-white/10 overflow-hidden gap-x-2'>
         {data.user.image ? (
           <Avatar>
             <AvatarImage src={data.user.image} />
